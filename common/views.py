30097b0319ee17be71304723d413eb9406c62f6d
ZhenYi Liang <sinerwr@gmail.com>
2016-01-26 17:16:53 +0800

    test

--- common/views.py
+++ common/views.py
@@ -86,8 +86,11 @@ def user_dashboard(request):
 def user_module(request, module, sid):
   if 'loginToken' in request.session:
     if module == "groups":
-      group_list = Group.objects.filter(uid = request.session['user_id'])
-      grpauth_list = GroupAuthorize.objects.select_related().filter(uid = request.session['user_id'])
+      group_list = GroupAuthorize.objects.select_related().filter(uid = request.session['user_id'])
+      if sid == "":
+        grpauth_list = group_list
+      else:
+        grpauth_list = GroupAuthorize.objects.select_related().filter(gid = sid)
       rsp = render(request, 'user_groups.html', locals())
       return HttpResponse(rsp)
     elif module == "assets":
@@ -103,3 +106,170 @@ def user_module(request, module, sid):
 def admin_module(request, module, sid):
   rsp = render(request, 'developing.html', locals())
   return HttpResponse(rsp)
+
+def data_execute(request):
+  result = {}
+  try:
+    module = request.POST['module']
+    action = request.POST['action']  
+  except:
+    module = ""
+    action = ""
+    result['code'] = 0
+    result['message'] = "拒绝非法访问"
+  if module == "group":
+    if action == "create":      
+      try:
+        name = request.POST['addGroup_name']
+        group = Group(name=name,
+                      uid=Account.objects.get(uid = request.session['user_id']),
+                      status=1,
+                    )
+        group.save()
+        inserted_gid = group.gid
+        groupauth = GroupAuthorize(gid = Group.objects.get(gid = inserted_gid),
+                                   uid = Account.objects.get(uid = request.session['user_id']),
+                                   privilege = 0,
+                                   )
+        groupauth.save()
+        result['code'] = 0
+        result['message'] = "创建组成功"
+      except:
+        result['code'] = 1
+        result['message'] = "创建组失败"
+    else:
+      pass
+  else:
+    pass
+  return HttpResponse(json.dumps(result), content_type="application/json")
+
+def rpc_api(request):
+  result = {}
+  if 'wechat_key' in request.POST and 'request' in request.POST:
+    api_token = request.POST['wechat_key']
+    api_request = request.POST['request']
+    api_command = api_request.split(' ')
+    if api_command[0] == "group":
+      if len(api_command) > 1:
+        api_subcommand = api_command[1]
+        try:
+          user = Account.objects.filter(wechat_token = api_token, status = 1)
+          user_id = user[0].uid
+          user_name = user[0].account
+        except:
+          user_id = ""
+        if user_id == "":
+          result['code'] = 1
+          result['message'] = "尚未绑定平台用户\n[绑定指令]bind account password"
+        else:
+          if api_subcommand == "list":
+            try:
+              group_list = GroupAuthorize.objects.select_related().filter(uid = user_id)
+              result['code'] = 0
+              result['group'] = {}
+              i = 0
+              for group in group_list:
+                if group.privilege == 0:
+                  group_auth = "管理"
+                elif group.privilege == 1:
+                  group_auth = "运维"
+                else:
+                  group_auth = "查看"
+                result['group'][i] = group.gid.name + " - " + group_auth
+                i = i + 1
+            except:
+              result['code'] = 1
+              result['message'] = "获取用户组失败，请确定是否已经创建用户组\n联系CSZ9227获取帮助"
+          else:
+            result['code'] = 1
+            result['message'] = "尚未支持的参数"
+      else:
+        result['code'] = 1
+        result['message'] = "group未带有参数\n详细帮助请输入 ?"
+    elif api_command[0] == "bind":
+      if len(api_command) == 3:
+        cherie_account = api_command[1]
+        cherie_password = api_command[2]
+        try:
+          user = Account.objects.filter(account = cherie_account, wechat_token__isnull = True)
+          user_id = user[0].uid
+        except:
+          user = ""
+        if user == "":
+          result['code'] = 1
+          result['message'] = "绑定失败：已绑定，账号不存在"
+        else:
+          if check_password(cherie_password,user[0].secpasswd):
+            try:
+              Account.objects.filter(uid = user_id).update(wechat_token = api_token)
+              result['code'] = 1
+              result['message'] = cherie_account + "用户绑定成功\n欢迎使用本平台"
+            except:
+              result['code'] = 1
+              result['message'] = "绑定失败：多账号绑定"
+          else:
+            result['code'] = 1
+            result['message'] = "绑定失败：密码错误"
+      else:
+        result['code'] = 1
+        result['message'] = "绑定参数出错,输入\nbind 运维平台用户名 运维平台密码"
+    else:
+      result['code'] = 1
+      result['message'] = "尚未支持的指令"
+  else:
+    result['code'] = 1
+    result['message'] = "非法操作"
+  return HttpResponse(json.dumps(result),content_type="application/json")
+
+def rpc_api_test(request):
+  result = {}
+  if 'wechat_key' in request.POST and 'request' in request.POST:
+    api_token = request.POST['wechat_key']
+    api_request = request.POST['request']
+    api_command = api_request.split(' ')
+    if api_command[0] == "group":
+      if len(api_command) > 1:
+        api_subcommand = api_command[1]
+        try:
+          user = Account.objects.filter(wechat_token = api_token, status = 1)
+          user_id = user[0].uid
+          user_name = user[0].account
+        except:
+          user_id = ""
+        if user_id == "":
+          result['code'] = 1
+          result['message'] = "尚未绑定平台用户\n[绑定指令]bind account password"
+        else:
+          if api_subcommand == "list":
+            try:
+              group_list = GroupAuthorize.objects.select_related().filter(uid = user_id)
+              result['code'] = 0
+              result['group'] = {}
+              i = 0
+              for group in group_list:
+                if group.privilege == 0:
+                  group_auth = "管理"
+                elif group.privilege == 1:
+                  group_auth = "运维"
+                else:
+                  group_auth = "查看"
+                result['group'][i] = group.gid.name + " - " + group_auth
+                i = i + 1
+            except:
+              result['code'] = 1
+              result['message'] = "获取用户组失败，请确定是否已经创建用户组\n联系CSZ9227获取帮助"
+          else:
+            result['code'] = 1
+            result['message'] = "尚未支持的参数"
+      else:
+        result['code'] = 1
+        result['message'] = "group未带有参数\n详细帮助请输入 ?"
+    elif api_command[0] == "bind":
+      pass
+    else:
+      result['code'] = 1
+      result['message'] = "尚未支持的指令"
+  else:
+    result['code'] = 1
+    result['message'] = "非法操作"
+  return HttpResponse(json.dumps(result),content_type="application/json")
